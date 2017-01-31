7d3b421489ba34bde59d784f569e80ad93cfe70d
Aleksandr Komlev <aleksandr.komlev@gmail.com>
2017-02-01 00:58:46 +0300

    Fixed issues #6, #8

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ import {exec, execSync} from 'child_process';
 import {createHash} from 'crypto';
 
 let {platform, arch}: Object = process,
-    win32RegBinPath = {
-        x64: '%windir%\SysWOW64',
-        ia32: '%windir%\System32'
-    },
     guid: Object = {
         darwin: 'ioreg -rd1 -c IOPlatformExpertDevice',
-        win32: `${win32RegBinPath[arch]}\REG ` +
+        win32: `%windir%\System32\REG ` +
             `QUERY HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Cryptography ` +
             `/v MachineGuid`,
-        linux: 'cat /var/lib/dbus/machine-id /etc/machine-id 2> /dev/null'
+        linux: 'cat /var/lib/dbus/machine-id /etc/machine-id 2> /dev/null || :'
     };
 
 function hash(guid: string): string {
@@ -49,9 +45,11 @@ export function machineIdSync(original: boolean): string {
 
 export function machineId(original: boolean): Promise<string> {
     return new Promise((resolve: Function, reject: Function): Object => {
-        return exec(guid[platform], (err: any, stdout: any, stderr: any): string => {
+        return exec(guid[platform], {}, (err: any, stdout: any, stderr: any) => {
             if (err) {
-                return reject(new Error(`Error while obtaining machine id: ${err.stack}`));
+                return reject(
+                    new Error(`Error while obtaining machine id: ${err.stack}`)
+                );
             }
             let id: string = expose(stdout.toString());
             return resolve(original ? id : hash(id));
